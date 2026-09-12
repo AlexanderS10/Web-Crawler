92ce994ac9facd681930dcf8a5261065241b0336
AlexanderS10 <alexander.sanchez.ps@live.com>
2026-09-12 19:12:15 -0400

    Added a normalize urls function that removes the duplication of urls

--- html_parser.py
+++ html_parser.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlsplit, urldefrag
+from urllib.parse import urljoin, urlsplit, urldefrag, urlunsplit
 import os
 
 BLACKLIST_EXTENSIONS = {
@@ -7,7 +7,22 @@ BLACKLIST_EXTENSIONS = {
     '.zip', '.tar', '.gz', '.rar', '.exe', '.bin','.mp3', '.mp4', '.avi', '.mov', '.mkv','.css', '.js', '.txt'
 }
 
-
+def normalize_url(url:str) -> dict[str,str]:
+    """
+    Normalize the urls to lower cases and remove fragments like class ids
+    
+    return a dict: url:str, path: str, scheme:str
+    """
+    
+    parts = urlsplit(url)
+    scheme = parts.scheme.lower()
+    domain = parts.netloc.lower()
+    path = parts.path
+    if not path and (scheme in ("http", "https")):
+        path = "/"
+    fragment = ""
+    return {"url":urlunsplit((scheme, domain, path, parts.query, fragment)), "path":path, "scheme":scheme}
+    
 def parse_html(html_content, absolute_url):
     """
     Here is where I will parse the html and only accept valid hrefs
@@ -18,15 +33,15 @@ def parse_html(html_content, absolute_url):
     base_tag = soup.find('base')
     base_url = ''
     if base_tag and base_tag.get('href') and isinstance(base_tag.get('href'),str):
-        base_url = urljoin(absolute_url, base_tag.get('href').strip())
+        base_url = urljoin(absolute_url, str(base_tag.get('href')).strip())
     else:
         base_url = absolute_url
     for link in soup.find_all('a'):
         link_str = link.get('href')
         if link_str and isinstance(link_str, str):
-            link_str = urldefrag(urljoin(base_url, link_str)).url
-            link_stripped = urlsplit(link_str)
-            root, ext = os.path.splitext(link_stripped.path)
-            if (link_stripped.scheme == "https" or link_stripped.scheme == "http") and ext.lower() not in BLACKLIST_EXTENSIONS:
+            normalized_url = normalize_url(urljoin(base_url, link_str)) 
+            link_str = normalized_url.get("url")
+            root, ext = os.path.splitext(normalized_url.get("path","/"))
+            if (normalized_url.get("scheme") == "https" or normalized_url.get("scheme") == "http") and ext.lower() not in BLACKLIST_EXTENSIONS:
                 links_set.add(link_str)
     return links_set
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fetcher import fetcher, RobotsCache
 from urllib.robotparser import RobotFileParser
 from queue import Queue
 from urllib.parse import urlsplit
-
+from html_parser import normalize_url
 
 def main():
     robots_cache = RobotsCache()
@@ -11,7 +11,8 @@ def main():
     limit = 10
     queue:Queue[tuple[str,int]]= Queue()
     counter =0
-    queue.put(("https://falexsanchez.com", 0))
+    seed_test = normalize_url("https://falexsanchez.com")
+    queue.put((seed_test["url"], 0))
     while counter < limit and not queue.empty():
         url, depth = queue.get()
         if url in seen_urls:
@@ -23,7 +24,9 @@ def main():
         counter+=1
         status_code, content_size, full_url, links = result
         print(f"{counter}. {full_url}, status:{status_code}, size: {content_size}, depth: {depth}")
-        if links:
+        if full_url not in seen_urls:
+            seen_urls.add(full_url) #THis is the final url in case of redirects so needs to be added to seen
+        if links: 
             for link in links:
                 depth +=1
                 queue.put((link, depth))
